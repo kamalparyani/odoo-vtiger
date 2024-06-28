4b4e070fee874f2f195b73d22864aead5787d7b0
Rohan <rohan.c.serpentcs@gmail.com>
2024-06-28 12:01:32 +0530

    [ADD] Added connector purchase module with updated code

--- vtiger_connector_purchase/models/res_company.py
+++ vtiger_connector_purchase/models/res_company.py
@@ -2,9 +2,6 @@
 
 import json
 from odoo import api, models
-from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DF
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DT
-from datetime import datetime
 from urllib.request import urlopen, Request
 from urllib.parse import urlencode
 
@@ -21,42 +18,41 @@ class ResCompany(models.Model):
            Because the Vtiger return dictionary'''
         purchase_order_obj = self.env['purchase.order']
         for res in result.get('result', []):
-            order_id = purchase_order_obj.search(
-                [('vtiger_id', '=', res.get('id'))], limit=1)
+            order_id = purchase_order_obj.search([('vtiger_id', '=', res.get('id'))], limit=1)
             if order_id:
                 order_id.order_line.unlink()
         return True
 
     def sync_vtiger_purchase_order(self):
+        purchase_order_obj = self.env['purchase.order']
+        partner_obj = self.env['res.partner']
+        product_obj = self.env['product.product']
         for company in self:
             # Synchronise Partner
             company.sync_vtiger_partner()
             # Synchronise Product
-            company.sync_vtiger_products()
+            company.sync_vtiger_products(company, vtiger_type=['Products','Services'])
+            # company.sync_vtiger_products()
             access_key = company.get_vtiger_access_key()
             session_name = company.vtiger_login(access_key)
             if company.last_sync_date:
-                qry = ("""SELECT * FROM PurchaseOrder
-                            WHERE modifiedtime >= '%s';"""
-                       % (company.last_sync_date))
+                qry = ("""SELECT * FROM PurchaseOrder WHERE modifiedtime >= '%s';"""% (company.last_sync_date))
             else:
                 qry = """SELECT * FROM PurchaseOrder;"""
             values = {'operation': 'query',
                       'query': qry,
                       'sessionName': session_name}
+
             data = urlencode(values)
             url = company.get_vtiger_server_url()
             req = Request('%s?%s' % (url, data))
             response = urlopen(req)
             result = json.loads(response.read())
-            purchase_order_obj = self.env['purchase.order']
-            partner_obj = self.env['res.partner']
-            product_obj = self.env['product.product']
+
             if result.get('success'):
                 self.update_existing_order(result)
                 for res in result.get('result', []):
-                    order_id = purchase_order_obj.search(
-                        [('vtiger_id', '=', res.get('id'))], limit=1)
+                    order_id = purchase_order_obj.search([('vtiger_id', '=', res.get('id'))], limit=1)
                     po_order_vals = {}
                     if not order_id:
                         contact_id = res.get('vendor_id')
@@ -68,38 +64,34 @@ class ResCompany(models.Model):
                                     {'partner_id': partner.id})
                         date_o = res.get('createdtime')
                         if date_o:
-                            awe = str(date_o)
-                            date_frm = datetime.strptime(awe, DT)
-                            date_order = date_frm.strftime(DT)
-                            po_order_vals.update({'date_order': date_order})
+                            po_order_vals.update({'date_order': date_o})
                         date_modified = res.get('modifiedtime')
                         if date_modified:
-                            modified = str(date_modified)
-                            modified_date = datetime.strptime(modified, DT)
-                            date_planned = modified_date.strftime(DF)
                             po_order_vals.update(
-                                {'date_planned': date_planned})
+                                {'date_planned': date_modified})
                         po_order_vals.update(
                             {'vtiger_id': res.get('id'),
                              'notes': res.get('terms_conditions')}),
                         order_id = purchase_order_obj.create(po_order_vals)
-                    product_id = res.get('productid')
-                    if product_id:
-                        product = product_obj.search(
-                            [('vtiger_id', '=', product_id)], limit=1)
-                    price_unit = res.get('listprice')
+
                     netprice = res.get('hdnGrandTotal')
-                    quantity = res.get('quantity')
-                    order_line_vals = {
-                        'name': res.get('comment'),
-                        'product_id': product and product.id or False,
-                        'product_uom': product.uom_id.id,
-                        'product_qty': float(quantity),
-                        'price_unit': float(price_unit),
-                        'price_subtotal': float(netprice),
-                        'order_id': order_id.id,
-                        'date_planned': order_id.date_order}
-                    if order_id:
-                        order_id.write(
-                            {'order_line': [(0, 0, order_line_vals)]})
+                    if res.get('lineItems'):
+                        for order_line_dict in res.get('lineItems'):
+                            product = product_obj.search([('vtiger_id', '=', order_line_dict.get('productid'))], limit=1)
+                            price_unit = order_line_dict.get('listprice')
+                            quantity = order_line_dict.get('quantity')
+
+                            order_line_vals = {
+                                'name': order_line_dict.get('comment'),
+                                'product_id': product and product.id,
+                                'product_uom': product.uom_id.id or 0,
+                                'product_qty': float(quantity) if quantity is not None else 0.00, # TypeError: float() argument must be a string or a number, not 'NoneType'
+                                'price_unit': float(price_unit) if price_unit is not None else 0.00,
+                                'price_subtotal': float(netprice) if netprice is not None else 0.00,
+                                'order_id': order_id.id,
+                                'date_planned': order_id.date_order.strftime("%Y-%m-%d %H:%M:%S")
+                            }
+                            if order_id:
+                                order_id.write(
+                                    {'order_line': [(0, 0, order_line_vals)]})
         return True
