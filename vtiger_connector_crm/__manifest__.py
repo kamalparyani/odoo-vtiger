285504673b72c8a064e34492db05dd9dcef9ab06
Rohan <rohan.c.serpentcs@gmail.com>
2024-06-28 11:50:50 +0530

    [ADD] Added connector crm module with updated code

--- vtiger_connector_crm/__manifest__.py
+++ vtiger_connector_crm/__manifest__.py
@@ -2,14 +2,13 @@
 
 {
     'name': 'VTiger Lead Connector',
-    'version': '13.0.1.0.0',
+    'version': '15.0.1.0.0',
     'category': 'Sales',
     'license': 'AGPL-3',
     'author': 'Serpent Consulting Services Pvt. Ltd.',
     'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
     'website': 'https://www.serpentcs.com',
-    'depends': ['crm',
-                'vtiger_connector_partner'],
+    'depends': ['crm','vtiger_connector_partner'],
     'data': ['views/res_company_view.xml',
              'views/crm_view.xml'],
     'installable': True,
