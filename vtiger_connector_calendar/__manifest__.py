f5af5b337333a5c634464bb2e14ecb233c15b676
Rohan <rohan.c.serpentcs@gmail.com>
2024-06-28 11:48:59 +0530

    [ADD] Added connector calendar module with updated code

--- vtiger_connector_calendar/__manifest__.py
+++ vtiger_connector_calendar/__manifest__.py
@@ -2,14 +2,13 @@
 
 {
     'name': 'VTiger Calendar Connector',
-    'version': '13.0.1.0.0',
+    'version': '15.0.1.0.0',
     'category': 'Marketing',
     'license': 'AGPL-3',
     'author': 'Serpent Consulting Services Pvt. Ltd.',
     'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
     'website': 'https://www.serpentcs.com',
-    'depends': ['calendar',
-                'vtiger_connector_base'],
+    'depends': ['calendar','vtiger_connector_base'],
     'data': ['views/res_company_view.xml',
              'views/calendar_views.xml'],
     'installable': True,
