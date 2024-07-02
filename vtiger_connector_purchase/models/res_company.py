0bdf11a779ba98dd977eaea69f193c0efb7c77c3
Rohan <rohan.c.serpentcs@gmail.com>
2024-07-02 19:00:13 +0530

    [IMP] Added condition to sync also improved code for multiple PO rec

--- vtiger_connector_purchase/models/res_company.py
+++ vtiger_connector_purchase/models/res_company.py
@@ -27,12 +27,8 @@ class ResCompany(models.Model):
         purchase_order_obj = self.env['purchase.order']
         partner_obj = self.env['res.partner']
         product_obj = self.env['product.product']
+        user_obj = self.env['res.users']
         for company in self:
-            # Synchronise Partner
-            company.sync_vtiger_partner()
-            # Synchronise Product
-            company.sync_vtiger_products(company, vtiger_type=['Products','Services'])
-            # company.sync_vtiger_products()
             access_key = company.get_vtiger_access_key()
             session_name = company.vtiger_login(access_key)
             if company.last_sync_date:
@@ -54,6 +50,10 @@ class ResCompany(models.Model):
                 for res in result.get('result', []):
                     order_id = purchase_order_obj.search([('vtiger_id', '=', res.get('id'))], limit=1)
                     po_order_vals = {}
+                    if res.get('contact_id') or res.get('vendor_id'):
+                            partner_exist = partner_obj.search(['|', ('vtiger_id', '=', res.get('contact_id')), ('vtiger_id', '=', res.get('vendor_id'))], limit=2)
+                            if not partner_exist:
+                                company.sync_vtiger_partner()
                     if not order_id:
                         contact_id = res.get('vendor_id')
                         if contact_id:
@@ -62,6 +62,14 @@ class ResCompany(models.Model):
                             if partner:
                                 po_order_vals.update(
                                     {'partner_id': partner.id})
+                        else:
+                            vtiger_user = user_obj.search([('login', '=', 'vtigeruser@vtiger')])
+                            if not vtiger_user:
+                                vtiger_user = user_obj.create({
+                                    'name': 'VTiger-User',
+                                    'login': 'vtigeruser@vtiger',
+                                })
+                            po_order_vals.update({'partner_id': vtiger_user.partner_id.id})
                         date_o = res.get('createdtime')
                         if date_o:
                             po_order_vals.update({'date_order': date_o})
@@ -77,7 +85,11 @@ class ResCompany(models.Model):
                     netprice = res.get('hdnGrandTotal')
                     if res.get('lineItems'):
                         for order_line_dict in res.get('lineItems'):
+                            if type(order_line_dict) != dict:
+                                order_line_dict = res.get('lineItems').get(order_line_dict)
                             product = product_obj.search([('vtiger_id', '=', order_line_dict.get('productid'))], limit=1)
+                            if not product:
+                                company.sync_vtiger_products(company, vtiger_type=['Products', 'Services'])
                             price_unit = order_line_dict.get('listprice')
                             quantity = order_line_dict.get('quantity')
 
