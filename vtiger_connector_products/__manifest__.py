9e6b79b0d42b46aa2093d118ff9a8cf8e9ee5d39
Rohan <rohan.c.serpentcs@gmail.com>
2024-07-03 12:45:38 +0530

    [ADD] Added v16 connector product module

---
+++ vtiger_connector_products/__manifest__.py
@@ -0,0 +1,17 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'VTiger Products Connector',
+    'version': '16.0.1.0.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
+    'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
+    'website': 'https://www.serpentcs.com',
+    'depends': ['product',
+                'vtiger_connector_base'],
+    'data': ['views/res_company_view.xml',
+             'views/product_template_view.xml'],
+    'installable': True,
+    'images': ['static/description/banner.png'],
+}
